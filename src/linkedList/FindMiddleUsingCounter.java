743618dd979e3a0ad9989ae258630a3419d91d7b
Akhil David <akhildavid26@hotmail.com>
2018-06-28 14:08:48 -0700

    Find Middle in singly linked list Linux

--- src/linkedList/FindMiddleUsingCounter.java
+++ src/linkedList/FindMiddleUsingCounter.java
@@ -1,10 +1,9 @@
 package linkedList;
 import java.util.*;
 
-public class FindMiddleTraversingWholeList
-{
-	Node head;  /* head of list */
-   
+public class FindMiddleUsingCounter{
+    Node head;  /* head of list */
+    
     /*
      * Function for adding node at the end of singly Linked List
      */
@@ -14,17 +13,17 @@ public class FindMiddleTraversingWholeList
     		 * Case when no node in the list
     		 */
     		head = node;
-	    }else{
-	    	/*
+    	} else {
+    		/*
 	    	 * Case when one or more nodes are present
 	    	 */
-	    	Node temp = head;
-	    	while (temp.next != null)
-	    	temp = temp.next;
-	    	temp.next = node;
-	    	node.next=null;
-	  	}
- }
+    		Node temp = head;
+    		while (temp.next != null)
+    			temp = temp.next;
+    			temp.next = node;
+    	}
+    }
+	  
     public static void main(String args[]){
     	/*
     	 * 1) First enter the number of test cases you want to run the program
@@ -34,56 +33,49 @@ public class FindMiddleTraversingWholeList
     	 * 5) Next enter the number of elements for the second test case
     	 * 6) Repeat from step 2 then
     	 */
-        Scanner sc = new Scanner(System.in);
-		int numberOfTestCases=sc.nextInt();
+         Scanner sc = new Scanner(System.in);
+		 int numberOfTestCases=sc.nextInt();
 		 
-		while(numberOfTestCases>0){
+		 while(numberOfTestCases>0){
 			int numberOfNodes = sc.nextInt();
-			FindMiddleTraversingWholeList singlyLinkedList = new FindMiddleTraversingWholeList();
+			FindMiddleUsingCounter singlyLinkedList = new FindMiddleUsingCounter();
+			
 			int headNodeData=sc.nextInt();
 			Node head= new Node(headNodeData);
             singlyLinkedList.addToTheLast(head);
-            for (int i = 1; i < numberOfNodes; i++) 
-			{
+            for (int i = 1; i <numberOfNodes ; i++) {
 				int nodeData = sc.nextInt(); 
 				singlyLinkedList.addToTheLast(new Node(nodeData));
 			}
-         FindMiddleTraversingWholeListLogic obj = new FindMiddleTraversingWholeListLogic(); 
-       
-         System.out.println(obj.getMiddle(singlyLinkedList.head));
-         
-         numberOfTestCases--;
+          
+            FindMiddleUsingCountersLogic gfgobj = new FindMiddleUsingCountersLogic(); 
+            System.out.println(gfgobj.getMiddle(singlyLinkedList.head));
+            numberOfTestCases--;
 		}
-		
-		sc.close();
+		 sc.close();
 	}
 }
 
-class FindMiddleTraversingWholeListLogic
-{
+class FindMiddleUsingCountersLogic{
     /*
-     *  Function to find middle element a linked list
+     * Function to find middle element a linked list
+     * Will return second middle element if number of elements in the node are even
      */
-    int getMiddle(Node head)
-   {
+    int getMiddle(Node head){
+    	Node mid=head;
     	Node temp=head;
-    	int len=0;
+    	int counter=0;   
     	while(temp!=null) {
-    		len++;
+    		if(counter%2!=0) {
+    			mid=mid.next;
+    		}
+    		counter++;
     		temp=temp.next;
     	}
-    	temp=head;
-    	for (int i=1;i<=len/2;i++) {
-    		
-    		temp=temp.next;
-    	}
-    	
-    	return temp.data;
+    	return mid.data;
         /*
-         * Two loops  
-         * 1) One complete
-         * 2) One half
-         * Highly inefficient 
+         * One complete loop
+         * Still inefficient
          */
    }
 }
--- src/linkedList/FindMiddleUsingTwoPointers.java
+++ src/linkedList/FindMiddleUsingTwoPointers.java
@@ -2,88 +2,77 @@ package linkedList;
 
 import java.util.*;
 
-public class FindMiddleUsingTwoPointers
-{
-    Node head;  // head of list
-  
-    
-     public void addToTheLast(Node node) {
-  if (head == null) {
-   head = node;
-  } else {
-   Node temp = head;
-   while (temp.next != null)
-    temp = temp.next;
-   temp.next = node;
-  }
- }
+public class FindMiddleUsingTwoPointers{
+	
+	Node head;  /* head of list */
+	
+	/*
+     * Function for adding node at the end of singly Linked List
+     */
+    public void addToTheLast(Node node) {
+    	if (head == null) {
+    		head = node;
+    		/*
+    		 * Case when no node in the list
+    		 */
+    	}else{
+    		/*
+	    	 * Case when one or more nodes are present
+	    	 */
+    		Node temp = head;
+    		while (temp.next != null)
+    		temp = temp.next;
+    		temp.next = node;
+    	 }
+     }
 	  
-     /* Driver program to test above functions */
-    public static void main(String args[])
-    {
-       
-         
-        /* Constructed Linked List is 1->2->3->4->5->6->
-           7->8->8->9->null */
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
+    public static void main(String args[]){
+    	/*
+    	 * 1) First enter the number of test cases you want to run the program
+    	 * 2) Then enter the number of elements for the first test case
+    	 * 3) Then enter the elements
+    	 * 4) It will show you the middle for the first test case
+    	 * 5) Next enter the number of elements for the second test case
+    	 * 6) Repeat from step 2 then
+    	 */	
+    	Scanner sc = new Scanner(System.in);
+		int numberOfTestCases=sc.nextInt();
 		 
-		 while(t>0)
-         {
-			int n = sc.nextInt();
-			FindMiddleTraversingWholeList llist = new FindMiddleTraversingWholeList();
-			//int n=Integer.parseInt(br.readLine());
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
+		while(numberOfTestCases>0){
+			int numberOfNodes = sc.nextInt();
+			FindMiddleUsingTwoPointers singlyLinkedList= new FindMiddleUsingTwoPointers();
+			int nodeHeadData=sc.nextInt();
+			Node head= new Node(nodeHeadData);
+            singlyLinkedList.addToTheLast(head);
+            for (int i = 1; i < numberOfNodes; i++) {
+				int nodeData = sc.nextInt(); 
+				singlyLinkedList.addToTheLast(new Node(nodeData));
 			}
           
-        GFG_1 gfgobj = new GFG_1(); 
-		//llist.head = new GFG().Middle(llist.head);
-		System.out.println(gfgobj.getMiddle(llist.head));
-        
-		//llist.printList();
-		
-			t--;
+            FindMiddleUsingTwoPointersLogic gfgobj = new FindMiddleUsingTwoPointersLogic();
+            System.out.println(gfgobj.getMiddle(singlyLinkedList.head));
+			numberOfTestCases--;
 		}
 		 sc.close();
 	}
 }
 
-
-/*Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function is mentioned above.*/
-
-/* Node of a linked list
- class Node {
-   int data;
-    Node next;
-    Node(int d)  { data = d;  next = null; }
-}
- Linked List class
-class LinkedList
-{
-    Node head;  // head of list
-}
-This is method only submission.  You only need to complete the method. */
-class GFG_1
-{
-    // Function to find middle element a linked list
-    int getMiddle(Node head)
-   {
+class FindMiddleUsingTwoPointersLogic{
+	/*
+     * Function to find middle element a linked list
+     * Will return second middle element if number of elements in the node are even
+     */
+    int getMiddle(Node head){
     	Node slow_ptr=head;
     	Node fast_ptr=head;
-    	
     	while(fast_ptr!=null&&fast_ptr.next!=null) {
     		slow_ptr=slow_ptr.next;
     		fast_ptr=fast_ptr.next.next; 		
     	}
     	return slow_ptr.data;
-        
+        /*
+         * one loop 
+         * moving two pointers in each iteration
+         */
    }
 }
